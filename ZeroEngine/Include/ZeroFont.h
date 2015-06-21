9255bb114082f10e03a732722b47dd50239ef872
youngminz <youngmin@youngminz.kr>
2015-06-21 21:08:55 +0900

    수정된 ZeroFont.h 추가

--- ZeroEngine/Include/ZeroFont.h
+++ ZeroEngine/Include/ZeroFont.h
@@ -1,4 +1,8 @@
-﻿#pragma once
+﻿/**
+	@file ZeroFont.h
+*/
+
+#pragma once
 
 #include "ZeroIScene.h"
 #include "ZeroResource.h"
@@ -25,7 +29,10 @@ public:
 		@param size 사용할 글꼴의 크기를 설정합니다.
 		@param string 화면에 나타낼 문자열을 설정합니다.
 		@param fontname 옵션. 사용할 글꼴을 나타냅니다. 기본값은 맑은 고딕입니다.
-		@param filename 옵션. 글꼴의 경로를 나타냅니다. 시스템에 설치하지 않은 글꼴을 사용할 때 사용합니다. 기본값은 NULL입니다.
+			사용하고자 하는 글꼴에서 속성 > 자세히에 나오는 제목을 넣으면 됩니다.
+		@param filename 옵션. 글꼴의 경로를 나타냅니다. 
+			시스템에 설치하지 않은 글꼴을 사용할 때 사용합니다. 기본값은 NULL입니다.
+			사용할 수 있는 글꼴의 종류는 fon, fnt, ttf, ttc, fot, otf, mmm, pfb, pfm입니다.
 	*/
 	ZeroFont(UINT size, char* string, char* fontname = "맑은 고딕", char *filename = NULL);
 	~ZeroFont();
